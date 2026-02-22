6a3e9b8448372b01e5268bace80718ff5a80773e
Alexander Schischkin <schischkin2@yandex.com>
2026-02-22 19:28:56 +0100

    change generators.py

--- generators/generators.py
+++ generators/generators.py
@@ -9,3 +9,16 @@ def filter_by_currency(transactions, currency):
     for transaction in transactions:
         if transaction['operationAmount']['currency']['code'] == currency:
             yield transaction
+
+
+def transaction_descriptions(transactions):
+    """
+    Возвращает описания транзакций
+
+    :param transactions: список словарей с транзакциями
+    :return: итератор с описаниями
+    """
+    for transaction in transactions:
+        yield transaction.get('description', 'Нет описания')
+
+
