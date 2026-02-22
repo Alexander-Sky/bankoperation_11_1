3de3a1bd29351b84ac3753bc2f48cc6925601537
Alexander Schischkin <schischkin2@yandex.com>
2026-02-22 19:37:29 +0100

    changes in module test_generators.py

---
+++ tests/test_generators.py
@@ -0,0 +1,30 @@
+# Пример фикстуры с тестовыми данными
+@pytest.fixture
+def transactions():
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+# Остальные транзакции...
+    ]
+
+def test_filter_by_currency(transactions):
+    usd_transactions = filter_by_currency(transactions, "USD")
+    assert len(list(usd_transactions)) == 3
+
+def test_transaction_descriptions(transactions):
+    descriptions = transaction_descriptions(transactions)
+    expected = ["Перевод организации", "Перевод со счета на счет", ...]
+    assert list(descriptions) == expected
